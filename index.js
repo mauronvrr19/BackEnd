deeaa5debb677e4968a4abd42b21af17263832dd
mauronvrr19 <mauronvrr19@gmail.com>
2022-03-16 21:21:23 -0300

    desafios

---
+++ index.js
@@ -0,0 +1,81 @@
+const fs = require("fs")
+
+class container {
+    constructor(archivo){
+        this.archivo = archivo ;
+    }
+    async escribir(dato) {
+        try {
+            //  creo la promera y el dato es el json
+            await fs.promises.writeFile(this.archivo, dato)
+            console.log("todo ok")
+        }
+        catch(error) {
+            console.log (error)
+        }
+    }
+    async borrar(){
+        try{
+            // creo la promesa y le paso un array vacio
+            await fs.promises.writeFile(this.archivo, "[]")
+            console.log("borrado")
+        }
+        catch(error){ 
+throw new error (error)
+        }
+    }
+async leer () {
+    try{
+      const read =  await fs.promises.readFile(this.archivo,"utf-8")
+      console.log(JSON.parse(read))
+     return read
+    }
+    catch(error){   
+throw new error (error)
+    }
+}
+async id (id){ 
+    try{
+        const read =  await fs.promises.readFile(this.archivo,"utf-8")
+        const parceado = JSON.parse(read)
+        const mapa = console.table(parceado.find(prod =>prod.id == id))
+      }
+      catch(error){  
+            console.log (error)
+      }
+    
+}
+async borrarxid (id){
+    try{
+        const read =  await fs.promises.readFile(this.archivo,"utf-8")
+        const parceado = JSON.parse(read)
+        const mapa = console.table(parceado.filter(prod =>prod.id !== id))
+        return mapa
+      }
+      catch(error){  
+            console.log (error)
+      }
+
+}
+
+
+    async sobreescribir(nombre, apellido, id) {
+    const nuevo = {
+        nombre: nombre ,
+        apellido: apellido,
+        id: id,
+    }
+    try {
+        const cont = await fs.promises.readFile(this.archivo,"utf-8")
+        const now = JSON.parse(cont)
+        nuevo.id = now.length + 1;
+        now.push(nuevo)
+        fs.writeFileSync(this.archivo, JSON.stringify(now))
+    }
+    catch(e){
+        console.log(error)
+    }
+    }
+}
+
+module.exports = container;
---
+++ test.js
@@ -0,0 +1,24 @@
+const container = require("./index")
+
+
+// creo el archivo texto.txt 
+const contenido = new container("./texto.txt")
+
+// le pasamos un objeto el archivo texto.txt
+contenido.escribir (JSON.stringify(
+    [
+        {"nombre" : "lautaro" , "apellido":"rodriguez", "id" : 1},
+        {"nombre" : "mauro" , "apellido":"navarro", "id" : 2},
+        {"nombre" : "uriel" , "apellido":"zapata" , "id" : 3}
+    ]
+    )    )
+ 
+// contenido.borrar()
+contenido.leer() 
+contenido.id(3)
+contenido.borrarxid(1)
+contenido.sobreescribir (
+    "nata" , "messi" )
+
+
+
